ca88e5224f064da6eff689d3fdcb6046be06d9ee
yamifrankc <code@yamifrankc.com>
2024-04-03 02:50:57 -0500

    script

---
+++ generate-resume.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This script generates a html (and pdf) resume from a json file
+# by using JsonResume
+
+resume export resume.html --theme=./jsonresume-theme-onepage-plus/
+#resume export resume.pdf --theme=./jsonresume-theme-onepage-plus/

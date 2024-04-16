9ffb916f09bf25f746ff5190b58b884efa9f0de2
Francisco <francisco.trigomijes@gmail.com>
2024-04-16 00:34:04 -0500

    views

---
+++ updateViews.js
@@ -0,0 +1,16 @@
+let views = fetch('https://p0xwa3zuc0.execute-api.us-east-2.amazonaws.com/default/incrementResumeViews')
+.then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+document.body.innerHTML = "<p>Views are  " + views + "</p>"
+//   https://p0xwa3zuc0.execute-api.us-east-2.amazonaws.com/default/incrementResumeViews

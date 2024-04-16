17acf7a0537316ac6c09562f66e926f360a50653
Frank Trigo <code@trigomijes.com>
2024-04-16 14:03:37 -0500

    api

--- updateViews.js
+++ updateViews.js
@@ -1,21 +1,30 @@
-let views = fetch('https://p0xwa3zuc0.execute-api.us-east-2.amazonaws.com/default/incrementResumeViews')
-.then(res => res.json())
-.then(res => {document.getElementById("counter").innerHTML = res.body.count})
-.then(data => console.log(data));
+const apiURL = 'https://p0xwa3zuc0.execute-api.us-east-2.amazonaws.com/default/incrementResumeViews');
 
 
-// .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
+async function fetchIntegerFromAPI(apiUrl) {
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data from the API');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // You can handle the error as per your requirement
+    }
+  }
+  
+  // Example usage
+  let views = fetchIntegerFromAPI(apiUrl)
+    .then(integer => {
+      console.log('Integer value:', integer);
+      // Do something with the integer value
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  
 
 document.body.innerHTML = "<p>Views are  " + views + "</p>"
 //   https://p0xwa3zuc0.execute-api.us-east-2.amazonaws.com/default/incrementResumeViews

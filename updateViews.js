78b6e69400f678c5b71f8ca2d03409fb30897983
Francisco Trigo <francisco.trigomijes@gmail.com>
2024-04-16 13:39:24 -0500

    Update updateViews.js

--- updateViews.js
+++ updateViews.js
@@ -1,16 +1,21 @@
 let views = fetch('https://p0xwa3zuc0.execute-api.us-east-2.amazonaws.com/default/incrementResumeViews')
-.then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+.then(res => res.json())
+.then(res => => {document.getElementById("counter").innerHTML = res.body.count})
+.then(data => console.log(data));
+
+
+// .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
 
 document.body.innerHTML = "<p>Views are  " + views + "</p>"
 //   https://p0xwa3zuc0.execute-api.us-east-2.amazonaws.com/default/incrementResumeViews
